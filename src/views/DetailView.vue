343081eca11a1955119e0d2210d2989292c4a9a2
KhlangRak <best.0991327722@gmail.com>
2023-01-10 20:55:50 +0700

    details

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,3 +1,28 @@
-<template>Hello, Detail View</template>
+<template>
+  <div class="Header">
+    <img v-for="image in food.images" :src="image" alt="" />
+    <h1>{{ food.name }}</h1>
+    <div class="Section" v-for="section,head in food.sections">
+      <h2>{{ head }}</h2>
+      <p v-for="item in section">{{ item }}</p>
+    </div>
+  </div>
+</template>
 
-<style></style>
+<script>
+import db from "../database.json";
+export default{
+  data(){
+    food:{}
+  },
+  created(){
+    this.food = db.foods.find(food => food.id == this.$route.params.id) 
+  }
+}
+</script>
+
+<style scoped>
+* {
+  color: #000;
+}
+</style>
